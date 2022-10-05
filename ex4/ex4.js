b359197ef3711bf1d4f4533d19a86d7044e87330
wassel aguech <wasselaguech5@gmail.com>
2022-10-05 14:05:22 +0200

    update ex1 et ex4

--- ex4/ex4.js
+++ ex4/ex4.js
@@ -11,9 +11,9 @@ document.write(" le valeure de b est " + c ," et le type de c est " +typeof(c));
 
 
 
-a = Number(a)
-b=Number(b)
-c=Number(c)
+a = Number(a);
+b=Number(b);
+c=Number(c);
 
 document.write(" le valeure de a est " + a ," et le type de a est " +typeof(a));
 document.write(" le valeure de b est " + b ," et le type de b est " +typeof(b));
