0fca9406a8c4b3c16eb4d547e1be752c965308cc
wassel aguech <wasselaguech5@gmail.com>
2022-10-05 14:52:01 +0200

    update6

--- ex6/ex6.js
+++ ex6/ex6.js
@@ -1,17 +1,7 @@
-
-
-
-console.log('hello');
- var x= Math.floor(Math.random()*101);
-
-
-
-
+var x= Math.floor(Math.random()*101);
 var coups=0;
 do {
-
     y= Number(prompt('sasir un entier y'))
-
     coups++;
     if(x>y){
         alert('plus grand');
@@ -19,7 +9,6 @@ do {
     if(x<y){
         alert('plus petit');
     }
-    
 } while (x!=y );
 
 alert(`le nombre est trouve dans ${coups} fois`)
